a346daecc09ce3a9ac5179391568de2586e0fb86
lukaqueres <lkocieln@gmail.com>
2023-07-18 17:42:57 +0200

    Add a little thing with time :D

--- renderer.js
+++ renderer.js
@@ -1,9 +1,19 @@
-document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+if ( document.getElementById('toggle-dark-mode') ) {
+  document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
     const isDarkMode = await window.darkMode.toggle()
     document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
   })
-  
+}
+
+if ( document.getElementById('reset-to-system') ) {
   document.getElementById('reset-to-system').addEventListener('click', async () => {
     await window.darkMode.system()
     document.getElementById('theme-source').innerHTML = 'System'
   })
+}
+
+const d = new Date();
+let hour = d.getHours();
+if ( document.querySelector("#say-hello") ) {
+  document.querySelector("#say-hello").textContent = ( ( d < 6 || d > 18 ) ? "Good Night" : (( d < 12 )? "Good Morning" : "Good day"))
+}
